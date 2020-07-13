639302aded83d85d3524f4197cf241c097e6ba7c
Andrew Monshizadeh <amonshiz@gmail.com>
2020-07-13 11:57:32 -0400

    Initial Commit

---
+++ Shared/ToddlertainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToddlertainmentApp.swift
+//  Shared
+//
+//  Created by Andrew Monshizadeh on 7/13/20.
+//
+
+import SwiftUI
+
+@main
+struct ToddlertainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
