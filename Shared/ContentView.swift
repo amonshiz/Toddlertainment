ddfe17494c3b5c0b27828c2fe9ce162bc01f308d
Andrew Monshizadeh <amonshiz@gmail.com>
2020-07-13 13:11:34 -0500

    Use PointOfInterest in MapView to add annotations

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,9 +10,14 @@ import SwiftUI
 struct ContentView: View {
   @State var isPresented = false
 
+  let pointsOfInterest = [
+    PointOfInterest(name: "Church Square Park", coordinate: CGPoint(x: 40.742200, y: -74.032387), description: nil),
+    PointOfInterest(name: "Steven's Park", coordinate: CGPoint(x: 40.741468, y: -74.028031), description: nil),
+  ]
+
   var body: some View {
     NavigationView {
-      MapView()
+      MapView(pointsOfInterest)
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
         .toolbar {
           ToolbarItem(placement: .navigationBarLeading) {
--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -9,10 +9,22 @@ import SwiftUI
 import MapKit
 
 private struct MapViewImplementation<Context> {
+  let points: [PointOfInterest]
+
+  init(_ points: [PointOfInterest] = []) {
+    self.points = points
+  }
+
   func makeView(context: Context) -> MKMapView {
     let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.742218, longitude: -74.032312), latitudinalMeters: 3500, longitudinalMeters: 500)
     let view = MKMapView(frame: .zero)
     view.setRegion(region, animated: false)
+
+    view.addAnnotations(points.map({ poi in
+      let annotation = MKPointAnnotation()
+      annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(poi.coordinate.x), longitude: CLLocationDegrees(poi.coordinate.y))
+      return annotation
+    }))
 
     return view
   }
@@ -26,8 +38,8 @@ private struct MapViewImplementation<Context> {
 struct MapView: UIViewRepresentable {
   fileprivate let impl: MapViewImplementation<UIViewRepresentableContext<Self>>
 
-  init() {
-    impl = MapViewImplementation<UIViewRepresentableContext<Self>>()
+  init(_ points: [PointOfInterest] = []) {
+    impl = MapViewImplementation<UIViewRepresentableContext<Self>>(points)
   }
 
   func makeUIView(context: Context) -> MKMapView {
@@ -44,8 +56,8 @@ struct MapView: UIViewRepresentable {
 struct MapView: NSViewRepresentable {
   fileprivate let impl: MapViewImplementation<NSViewRepresentableContext<Self>>
 
-  init() {
-    impl = MapViewImplementation<NSViewRepresentableContext<Self>>()
+  init(_ points: [PointOfInterest] = []) {
+    impl = MapViewImplementation<NSViewRepresentableContext<Self>>(points)
   }
 
   func makeNSView(context: Context) -> MKMapView {
