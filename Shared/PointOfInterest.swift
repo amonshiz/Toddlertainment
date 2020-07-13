adb7114fd615b6ac92c9d0df744dd53093d626da
Andrew Monshizadeh <amonshiz@gmail.com>
2020-07-13 13:11:16 -0500

    Add PointOfInterest object for wrapping up content to highlight

---
+++ Shared/PointOfInterest.swift
@@ -0,0 +1,15 @@
+//
+//  PointOfInterest.swift
+//  Toddlertainment
+//
+//  Created by Andrew Monshizadeh on 7/13/20.
+//
+
+import Foundation
+import CoreGraphics
+
+struct PointOfInterest {
+  let name: String
+  let coordinate: CGPoint
+  let description: String?
+}
